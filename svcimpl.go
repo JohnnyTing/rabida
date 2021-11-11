832d665e264f4b8368e16e571b6295a7ea4eae10
wubin1989 <328454505@qq.com>
2021-11-11 16:12:19 +0800

    add more flags to headless chrome

--- svcimpl.go
+++ svcimpl.go
@@ -27,10 +27,25 @@ type RabidaImpl struct {
 
 func screenshot(ctx context.Context, out string, pageNo int) (err error) {
 	var buf []byte
-	if err = chromedp.Run(ctx, chromedp.FullScreenshot(&buf, 100)); err != nil {
+	timeoutCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	if err = chromedp.Run(timeoutCtx, chromedp.FullScreenshot(&buf, 100)); err != nil {
 		return errors.Wrap(err, "")
 	}
 	if err = ioutil.WriteFile(filepath.Join(out, fmt.Sprintf("screenshot_%d.png", pageNo)), buf, 0644); err != nil {
+		return errors.Wrap(err, "")
+	}
+	return nil
+}
+
+func writeHtml(ctx context.Context, out string, pageNo int) (err error) {
+	var html string
+	timeoutCtx, cancel := context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+	if err = chromedp.Run(timeoutCtx, chromedp.OuterHTML("html", &html, chromedp.ByQuery)); err != nil {
+		return errors.Wrap(err, "")
+	}
+	if err = ioutil.WriteFile(filepath.Join(out, fmt.Sprintf("index_%d.html", pageNo)), []byte(html), 0644); err != nil {
 		return errors.Wrap(err, "")
 	}
 	return nil
@@ -62,6 +77,30 @@ func (r RabidaImpl) CrawlWithConfig(ctx context.Context, job Job, callback func(
 			chromedp.NoFirstRun,
 			chromedp.NoDefaultBrowserCheck,
 			chromedp.NoSandbox,
+			chromedp.DisableGPU,
+			chromedp.Flag("disable-web-security", true),
+			chromedp.Flag("disable-background-networking", true),
+			chromedp.Flag("enable-features", "NetworkService,NetworkServiceInProcess"),
+			chromedp.Flag("disable-background-timer-throttling", true),
+			chromedp.Flag("disable-backgrounding-occluded-windows", true),
+			chromedp.Flag("disable-breakpad", true),
+			chromedp.Flag("disable-client-side-phishing-detection", true),
+			chromedp.Flag("disable-default-apps", true),
+			chromedp.Flag("disable-dev-shm-usage", true),
+			chromedp.Flag("disable-extensions", true),
+			chromedp.Flag("disable-features", "site-per-process,Translate,BlinkGenPropertyTrees"),
+			chromedp.Flag("disable-hang-monitor", true),
+			chromedp.Flag("disable-ipc-flooding-protection", true),
+			chromedp.Flag("disable-popup-blocking", true),
+			chromedp.Flag("disable-prompt-on-repost", true),
+			chromedp.Flag("disable-renderer-backgrounding", true),
+			chromedp.Flag("disable-sync", true),
+			chromedp.Flag("force-color-profile", "srgb"),
+			chromedp.Flag("metrics-recording-only", true),
+			chromedp.Flag("safebrowsing-disable-auto-update", true),
+			chromedp.Flag("enable-automation", true),
+			chromedp.Flag("password-store", "basic"),
+			chromedp.Flag("use-mock-keychain", true),
 		}
 		opts = append(opts, options...)
 		if conf.Mode == "headless" {
@@ -171,6 +210,9 @@ func (r RabidaImpl) CrawlWithConfig(ctx context.Context, job Job, callback func(
 		if err = screenshot(ctx, out, pageNo); err != nil {
 			return errors.Wrap(err, "")
 		}
+		if err = writeHtml(ctx, out, pageNo); err != nil {
+			return errors.Wrap(err, "")
+		}
 	}
 
 	ret, nextPageUrl, err = r.extract(ctx, job, conf)
@@ -224,6 +266,9 @@ func (r RabidaImpl) CrawlWithConfig(ctx context.Context, job Job, callback func(
 		pageNo++
 		if r.conf.Debug {
 			if err = screenshot(ctx, out, pageNo); err != nil {
+				return errors.Wrap(err, "")
+			}
+			if err = writeHtml(ctx, out, pageNo); err != nil {
 				return errors.Wrap(err, "")
 			}
 		}
