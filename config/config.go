53bbf5b17c373b022ba166b2657c7cede468fbd1
JohnnyTing <JohnnyTing2DingXu@gmail.com>
2022-04-21 13:48:07 +0800

    feat: update

--- config/config.go
+++ config/config.go
@@ -1,34 +1,37 @@
 package config
 
 import (
+	"time"
+
 	"github.com/kelseyhightower/envconfig"
 	"github.com/sirupsen/logrus"
-	"time"
 )
+
+//go:generate go-doudou name --file $GOFILE
 
 type RabiConfig struct {
 	// Delay the next request between from and to milliseconds
 	// if only from is specified, delay exactly from milliseconds
-	Delay []time.Duration `default:"2s,3s"`
+	Delay []time.Duration `default:"2s,3s" json:"delay"`
 	// Concurrency limits concurrency
 	// TODO
-	Concurrency int
+	Concurrency int `json:"concurrency"`
 	// ThrottleNum limits the request rate to ThrottleNum per ThrottleDuration
 	// TODO
-	ThrottleNum      int           `split_words:"true"`
-	ThrottleDuration time.Duration `split_words:"true"`
+	ThrottleNum      int           `split_words:"true" json:"throttleNum"`
+	ThrottleDuration time.Duration `split_words:"true" json:"throttleDuration"`
 	// Timeout specify a timeout for each page loading and element lookup
-	Timeout time.Duration `default:"10s"`
+	Timeout time.Duration `default:"10s" json:"timeout"`
 	// Mode Headless or browser
-	Mode string `default:"headless"`
+	Mode string `default:"headless" json:"mode"`
 	// Debug if true, it will take full screenshot of every page and output debug logs to stdout
-	Debug bool
+	Debug bool `json:"debug"`
 	// Out screenshot png files output path
-	Out string `default:"out"`
+	Out string `default:"out" json:"out"`
 	// Strict useragent、browser、platform should be matched, platform should not be linux etc..
-	Strict bool
+	Strict bool `json:"strict"`
 	//ProxyServer proxy server
-	Proxy string
+	Proxy string `json:"proxy"`
 }
 
 func LoadFromEnv() *RabiConfig {
--- svc.go
+++ svc.go
@@ -2,79 +2,82 @@ package service
 
 import (
 	"context"
+
 	"github.com/JohnnyTing/rabida/config"
 	"github.com/chromedp/chromedp"
 )
 
+//go:generate go-doudou name --file $GOFILE
+
 type CssSelector struct {
-	Css string
+	Css string `json:"css"`
 	// Attr default is innerText
-	Attr string
+	Attr string `json:"attr"`
 	// Scope supply a scope to each selector
 	// In jQuery, this would look something like this: $(scope).find(selector)
-	Scope string
+	Scope string `json:"scope"`
 	// Attrs map each attribute to a css selector. when Attrs equals nil, stop recursively populating
-	Attrs map[string]CssSelector
+	Attrs map[string]CssSelector `json:"attrs"`
 	// Iframe if true, we will look for the element(s) within the first iframe in the page. if IframeSelector exist, will look for this.
-	Iframe bool
+	Iframe bool `json:"iframe"`
 	// IframeSelector specify the iframe selector if have multiple iframe elements
-	IframeSelector *CssSelector
+	IframeSelector *CssSelector `json:"iframeSelector"`
 	// XpathScope Note: only choose one between xpath and css selector
-	XpathScope string
+	XpathScope string `json:"xpathScope"`
 	// Xpath xpath expression
 	// eg: //*[@id="zz"]/div[2]/ul/li[1]/text()
 	// eg: //div[@id="indexCarousel"]//div[@class="item"]//img/@src
-	Xpath    string
-	SetAttrs []SetAttribute
+	Xpath    string         `json:"xpath"`
+	SetAttrs []SetAttribute `json:"setAttrs"`
 	// Before dosomething before retrieve value
-	Before    []EventSelector
-	Condition *Condition
+	Before    []EventSelector `json:"before"`
+	Condition *Condition      `json:"condition"`
 }
 
 type Job struct {
 	// Link the url you want to crawl
-	Link string
+	Link string `json:"link"`
 	// CssSelector root css selector
-	CssSelector CssSelector
+	CssSelector CssSelector `json:"cssSelector"`
 	// PrePaginate do something before paginate
-	PrePaginate []EventSelector
+	PrePaginate []EventSelector `json:"prePaginate"`
 	// Paginator css selector for next page
-	Paginator     CssSelector
-	PaginatorFunc func(currentPageNo int) CssSelector
+	Paginator     CssSelector                         `json:"paginator"`
+	PaginatorFunc func(currentPageNo int) CssSelector `json:"paginatorFunc"`
 	// Limit limits how many pages should be crawled
-	Limit         int
-	StartPageBtn  CssSelector
-	StartPageUrl  string
-	EnableCookies HttpCookies
+	Limit         int         `json:"limit"`
+	StartPageBtn  CssSelector `json:"startPageBtn"`
+	StartPageUrl  string      `json:"startPageUrl"`
+	EnableCookies HttpCookies `json:"enableCookies"`
 }
 
 type EventSelector struct {
-	Type      Event
-	Condition Condition
-	Selector  CssSelector
+	Type      Event       `json:"type"`
+	Condition Condition   `json:"condition"`
+	Selector  CssSelector `json:"selector"`
 }
 
 type HttpCookies struct {
-	RawCookies string
-	Domain     string
+	RawCookies string `json:"rawCookies"`
+	Domain     string `json:"domain"`
 	// Expires hour, default 1 year
-	Expires int
+	Expires int `json:"expires"`
 }
 
 type SetAttribute struct {
-	AttributeName  string
-	AttributeValue string
+	AttributeName  string `json:"attributeName"`
+	AttributeValue string `json:"attributeValue"`
 }
 
 type Condition struct {
-	Value        string
-	CheckFunc    func(text, value string) bool
-	ExecSelector ExecSelector
+	Value        string                        `json:"value"`
+	CheckFunc    func(text, value string) bool `json:"checkFunc"`
+	ExecSelector ExecSelector                  `json:"execSelector"`
 }
 
 type ExecSelector struct {
-	Type     Event
-	Selector CssSelector
+	Type     Event       `json:"type"`
+	Selector CssSelector `json:"selector"`
 }
 
 type Event string
